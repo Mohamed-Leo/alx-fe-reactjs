feeca6c33ce1ec0b36e6a488b3461dc826378245
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-08-15 02:55:37 +0300

    uploading the alx-react-app-new files

---
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Welcome to ALX React APP!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>Hello everyone, I am learning React at ALX!</p>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
