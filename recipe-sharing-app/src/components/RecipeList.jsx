a6e3ee53cf93e6f2438b2022c90a63a9a161c338
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-08-25 01:41:33 +0300

    editing on recipe

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,11 +1,14 @@
 // RecipeList component
 import { useRecipeStore } from './recipeStore';
+import { SearchBar } from './SearchBar';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
     const recipes = useRecipeStore(state => state.filteredRecipes);
 
     return (
         <div>
+            <SearchBar />
             {recipes.map(recipe => (
                 <div key={recipe.id}>
                     <h3>{recipe.title}</h3>
