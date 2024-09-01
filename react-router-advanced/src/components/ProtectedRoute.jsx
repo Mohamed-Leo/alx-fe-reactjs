ad4125ad14f5ee49e6df53980e70a3110ea51f82
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-09-01 21:17:01 +0300

    implement useAuth custom hook

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,8 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import useAuth from '../custom-hooks/useAuth';
 
 function ProtectedRoute() {
-    // const isAuthenticated = localStorage.getItem("userToken") && true;
-    const isAuthenticated = true;
+    const isAuthenticated = useAuth();
 
     return isAuthenticated ? <Outlet /> : <Navigate to = "login"/>;
 }
---
+++ react-router-advanced/src/custom-hooks/useAuth.js
@@ -0,0 +1,9 @@
+
+
+function useAuth() {
+    // const isAuthenticated = localStorage.getItem("userToken") && true;
+    const isAuthenticated = true;
+    return isAuthenticated;
+}
+
+export default useAuth;
