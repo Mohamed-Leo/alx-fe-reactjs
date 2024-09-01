bfa1af2f68e956eda05ce2e7c4f497199bec504d
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-09-01 19:55:27 +0300

    adding basic validation in RegistrationForm

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -8,6 +8,12 @@ const RegistrationForm = () => {
         password: '',
     });
 
+    const [errors , setErrors] = useState({
+        nameError: '',
+        emailError: '',
+        passwordError: '',
+    });
+
     const {username ,email , password} = formData;
 
     const handleChange = (e) => {
@@ -20,19 +26,27 @@ const RegistrationForm = () => {
         console.log(formData);
     }
 
+
+    if (!username) setErrors({...errors , nameError : "name is required"})
+    if (!email) setErrors({...errors , emailError : "email is required"})
+    if (!password) setErrors({...errors , passwordError : "password is required"})
+
     return (
         <form onSubmit={handleSubmit}>
             <input type="text" name='username' placeholder='username'
             onChange={handleChange}
             value={username}/>
+            {errors.nameError && <span>{errors.nameError}</span>}
 
             <input type="email" name='email' placeholder='email'
             onChange={handleChange}
             value={email}/>
+            {errors.emailError && <span>{errors.emailError}</span>}
 
             <input type="password" name='password' placeholder='password'
             onChange={handleChange}
             value={password}/>
+            {errors.passwordError && <span>{errors.passwordError}</span>}
 
             <button type='submit'>submit</button>
         </form>
