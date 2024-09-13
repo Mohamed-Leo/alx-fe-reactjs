a3669dc189f9f934d5f9751581b26e15876d3333
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-09-13 19:34:00 +0300

    Building the Home Page of the Recipe Sharing Platform and modyfing the index css

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,10 +1,11 @@
+import HomePage from "./components/HomePage"
 
 
 function App() {
 
   return (
     <>
-      <h1 className="text-blue-500">hello tailwind</h1>
+      <HomePage />
     </>
   )
 }
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+function HomePage() {
+
+    const [recipes, setRecipes] = useState([]); 
+
+    useEffect(() => {
+        fetch('src/data.json')
+        .then((response) => response.json())
+        .then((data) => setRecipes(data))
+        .catch((error) => console.error('Error fetching recipes:', error));
+    });
+
+    return (
+        <div className="container mx-auto mt-10">
+            <h1 className="text-4xl font-bold text-center mb-8">Recipe Sharing Platform</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-3">
+                {recipes?.map((recipe) => (
+                    <div
+                    key={recipe.id}
+                    className="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
+                    >
+                        <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
+                        <div className="p-4">
+                            <h2 className="text-xl font-semibold mb-2">{recipe.title}</h2>
+                            <p className="text-gray-600">{recipe.summary}</p>
+                            <a
+                            href={`/recipes/${recipe.id}`}
+                            className="text-blue-500 hover:text-blue-700 mt-4 inline-block"
+                            >
+                            View Recipe
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
