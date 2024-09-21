512046e9808aaa3bdc84143f07d05527dffcdecc
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-09-21 06:47:16 +0300

    adding tailwind

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -4,8 +4,8 @@ import Search from "./components/Search";
 function App() {
 
   return (
-    <div className="container">
-      <h1>GitHub User Search</h1>
+    <div>
+      <h1 className="text-center my-8 text-3xl">GitHub User Search</h1>
       <Search />
     </div>
   );
--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import fetchUserData from "../services/githubService";
 
-
 function Search() {
-    const [username, setUsername] = useState('');
+    const [username, setUsername] = useState("");
     const [userData, setUserData] = useState(null);
-    const [loading , setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
 
     const handleSearch = (e) => {
@@ -16,48 +15,56 @@ function Search() {
 
         // show loading---
         setLoading(true);
+        setError("");
 
         // call the fetchUserData by service----
         const data = fetchUserData(username);
-        data.then(res => {
-            setUserData(res)
-            setLoading(false);
-            setError(null);
-        })
-        .catch(() => {
-            setError("Looks like we cant find the user");
-            setLoading(false);
-            setUserData(null);
-        })
-    }
+        data
+            .then((res) => {
+                setUserData(res);
+                setLoading(false);
+                setError(null);
+            })
+            .catch(() => {
+                setError("Looks like we cant find the user");
+                setLoading(false);
+                setUserData(null);
+            });
+    };
 
     return (
-        <div>
-            <form onSubmit={handleSearch}>
+        <div className="w-fit mx-auto">
+            <form onSubmit={handleSearch} className="space-x-4 text-center">
                 <input
-                type="text"
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-                placeholder="Enter GitHub username"
+                    className="py-2 px-3 rounded-md"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Enter GitHub username"
                 />
-                <button type="submit">Search</button>
+                <button className="bg-blue-700 py-1 px-3 rounded-md" type="submit">
+                    Search
+                </button>
             </form>
 
-            {loading && <p>Loading...</p>}
-            {error && <p>{error}</p>}
+            {loading && <p className="text-center mt-5">Loading...</p>}
+            {error && <p className="text-center text-red-700 mt-5">{error}</p>}
 
-            {
-                userData && (
-                    <div>
-                        <img src={userData.avatar_url} alt={userData.login} width="100" />
-                        <h3>{userData.name}</h3>
-                        <p>{userData.bio}</p>
-                        <a href={userData.html_url} target="_blank">
-                            Visit GitHub Profile
-                        </a>
-                    </div>
-                )
-            }
+            {userData && (
+                <div className="text-center space-y-3 mt-10">
+                    <img
+                        className="mx-auto rounded-md"
+                        src={userData.avatar_url}
+                        alt={userData.login}
+                        width="100"
+                    />
+                    <h3>{userData.name}</h3>
+                    <p>{userData.bio}</p>
+                    <a className="mt-5 block text-purple-700 underline" href={userData.html_url} target="_blank">
+                        Visit GitHub Profile
+                    </a>
+                </div>
+            )}
         </div>
     );
 }
